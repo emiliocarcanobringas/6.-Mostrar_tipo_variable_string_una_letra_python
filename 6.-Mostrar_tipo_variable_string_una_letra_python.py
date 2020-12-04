0ffed4ceddbd89edf770d72611d6b624723a0c42
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 19:47:47 -0600

    Add files via upload

---
+++ 6.-Mostrar_tipo_variable_string_una_letra_python.py
@@ -0,0 +1,17 @@
+# Este programa muestra en pantalla el valor de una variable string de una sola letra
+
+print("Este programa muestra en pantalla el valor de una variable string de una sola letra")
+print("Y nos muestra el tipo de variable, lo mostramos con comillas simples y comillas dobles")
+cadena = 'a'
+cadena2 = "ñ"
+print("El valor de la cadena es: ")
+print(cadena)
+print("Es una variable de tipo: ")
+print(type(cadena))
+print("El valor de la cadena 2 es: ")
+print(cadena2)
+print("Es una variable de tipo: ")
+print(type(cadena2))
+print("Este programa fue escrito por Emilio Carcaño Bringas")
+
+#Este programa fue escrito por Emilio Carcaño Bringas
